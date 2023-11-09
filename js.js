2dc1d3d824d9b0987d6ca589df5ae57701c9082d
Ilnaz <bakumov823@mail.ru>
2023-11-09 13:34:46 +0500

    Первый коммит

---
+++ js.js
@@ -0,0 +1,20 @@
+
+// const app = ['ilna', '1', 'false']
+
+// console.log(app[0])
+
+// // alert("Hello World")
+// // const perl = confirm("are you here?");
+
+// // console.log(perl);
+ 
+// const milf = prompt("Есть ли вам 18?", "Yes");
+
+// console.log(typeof(milf));
+
+let Input = 10,
+        referf = 11;
+
+
+console.log(Input++);
+console.log(referf--);
